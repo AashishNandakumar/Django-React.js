98841aa698704f448e3f1f9c1001546dfa159052
Aashish <aashishnkumar@gmail.com>
2023-12-08 20:18:58 +0530

    Updated Documentation

--- env/quotes/core/serializer.py
+++ env/quotes/core/serializer.py
@@ -11,3 +11,17 @@ class ReactSerializer(serializers.ModelSerializer):
     class Meta:
         model = React
         fields = ['name', 'detail']
+
+
+
+'''
+Serializers in Django REST framework serve two main purposes:
+
+1.They handle the conversion of complex data types (like model instances) 
+    into simpler, renderable formats (serialization), especially for 
+    outputting data in responses.
+2.They handle the conversion of incoming data (like JSON from a POST request) 
+    into Python data types, validate this data, and then use it to create or 
+    update model instances (deserialization).
+
+'''
